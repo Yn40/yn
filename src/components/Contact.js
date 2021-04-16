221eb66f0c2ca60f6f88d51e3427ec2aa1b2b5e1
Yn <lyn6264@gomcorp.com>
2021-04-16 18:07:11 +0900

    contact to me 완료

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,38 +1,80 @@
 import React, { useState } from 'react';
 import ico_mail from 'img/ico_mail.svg';
+import loading from "img/loading.svg";
 import emailjs from 'emailjs-com';
-import Popup from './Popup';
+
+const initialIpts = {
+	name : '',
+	email : '',
+	phone : '',
+	title : '',
+	message : '',
+}
+
 const Contact = () => {
-	const [open, setOpen] = useState(false);
-	const [value, setValue] = useState("");
-	const [sendState, setSendState] = useState(null);
+	const [ipts, setIpts] =useState(initialIpts);
+	const [mailState, setMailState] =useState({
+		oepn : false,
+		send : '',
+		loading : false,
+	});
+	// const [open, setOpen] = useState(false);
+	// const [sendState, setSendState] = useState(null);
 
 	const sendEmail = e => {
-    	e.preventDefault();
+		e.preventDefault();
 		e.stopPropagation();
 		console.log(e);
-		setValue("");
-		// emailjs.sendForm('yn8206', 'basic', e.target, 'user_bLEa1qqkN6v3iH5XoFW7F')
-		// .then((result) => {
-		// 	setOpen(true);
-		// 	setSendState("succes");
-		// 	console.log(result);
-		// }, (error) => {
-		// 	setSendState("error");
-		// 	throw Error (e);
-		// });
-  	}
-	const onClose = () =>{
-		setOpen(false);
+		
+		setMailState({
+			loading:true
+		});
+		emailjs.sendForm('yn8206', 'basic', e.target, 'user_bLEa1qqkN6v3iH5XoFW7F')
+		.then((result) => {
+			setMailState({
+				open:true,
+				send:"succes",
+				loading:false
+			});
+			setIpts(initialIpts);
+		}, (error) => {
+			setMailState({
+				open:true,
+				send:"error",
+				loading:false
+			});
+			throw Error (e);
+		});
 	}
+	const onClose = () =>{
+		setMailState({
+			open:false,
+		});
+	}
+	
+  const onChange = e =>{
+		console.log(e.target.name)
+		console.log(e.target.value)
+		setIpts({
+			[e.target.name] : e.target.value
+		})		
+	;}
+
 	return (
 		<section className="contact">
-			<h1>Contact to Me</h1>
+			<h1>Contact To Me</h1>
 			{
-				open?
+				mailState.loading ? <div className="loading"><img src={loading} alt="loading"/></div>:""
+			}
+			{
+				mailState.open?
 				<div className="alert">
-					<p>{sendState !== "error" ? "메일을 성공적으로 보냈습니다" : "메일을 보내기를 실패하였습니다"}</p>
-					<button type="button" onClick={onClose}>닫기</button>
+					<div className="cont">
+						{mailState.send !== "error" ? 
+						<p>메일을 성공적으로 보냈습니다.<br/>확인 후 답변 드리겠습니다.</p> 
+						: <p>메일을 보내기를 실패하였습니다<br/>다시 한번 시도해주세요.</p>}
+						<button className="btn_close" type="button" onClick={onClose}>닫기</button>
+					</div>
 				</div>
 				: ""
 			}
@@ -40,21 +82,28 @@ const Contact = () => {
 				<table className="tbl">
 					<tbody>
 						<tr>
-							<td><label>Name</label><input type="text" name="name" value={value} /></td>	
-							<td><label>E-Mail</label><input type="email" name="email" value={value} /></td>	
-							<td><label>Phone</label><input type="phone" name="phone" value={value} /></td>	
+							<td><label>Name</label><input type="text" name="name" placeholder="name" onChange={onChange} value={ipts.name} autoFocus /></td>	
+							<td><label>E-Mail</label><input type="email" name="email" placeholder="E-Mail"  onChange={onChange} value={ipts.email}/></td>	
+							<td><label>Phone</label><input type="phone" name="phone" placeholder="Phone Number"  onChange={onChange} value={ipts.phone}/></td>	
 						</tr>
 						<tr>
-							<td colSpan="3"><input type="text" name="title" value={value} /></td>
+							<td colSpan="3"><input type="text" name="title"  placeholder="제목" onChange={onChange} value={ipts.title}/></td>
 						</tr>
 						<tr>
-							<td colSpan="3"><textarea name="message" value={value} /></td>
+							<td colSpan="3"><textarea name="message" placeholder="내용을 입력해주세요."  onChange={onChange} value={ipts.message}/></td>
+						</tr>
+						<tr>
+							<td colSpan="3"><button type="submit" className="btn"><img src={ico_mail} className="icon" alt="메일 아이콘"/> 메일 보내기</button></td>
+						</tr>
+						<tr>
+							<td colSpan="3" className="noti">
+								📢 용량이 큰 메일은 전송이 안됩니다. 아래 메일로 따로 보내주시길 바랍니다.<br/>
+								<a href="mailto:lyn6264@naver.com"><b>lyn6264@naver.com</b></a>로 보내주세요. <br/>
+							</td>
 						</tr>
 					</tbody>
 				</table>	
-				<button type="submit" className="btn"><img src={ico_mail} className="icon" alt="메일 아이콘"/> 메일 보내기</button>
 			</form>
-			<a className="btn sm" href="mailto:lyn6264@naver.com">lyn6264@naver.com</a>
 		</section>
 	);
 };
