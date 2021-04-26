3b3dc1d850d6ecfdef4b261514527f555c270f56
Yuna <lyn6264@naver.com>
2021-04-26 22:13:10 +0900

    라우터 수정

--- src/Yn.js
+++ src/Yn.js
@@ -5,17 +5,49 @@ import Main from 'components/Main';
 import About from 'components/About';
 import Contact from 'components/Contact';
 import Portfolio from 'components/yn/Portfolio';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, Link, useLocation , useHistory } from 'react-router-dom';
+
+const routerPath = [
+  "/yn/about",
+  "/yn/contact",
+  "/yn/about",
+  "/yn/portfolio",
+]
 
 const Yn = () => {
+  function NoMatch() {
+    console.log("----NoMatch-----");
+    let location = useLocation();
+    let history = useHistory();
+    routerPath.map(url => {
+      if(url == location) {
+        alert("stop1");
+        history.replace(url);
+      }
+    });
+    alert("stop2");
+    history.replace("/yn");
+
+    
+    console.log(location);
+    return (
+      <div>
+        <h3>
+          No match for <code>{location.pathname}</code>
+        </h3>
+      </div>
+    );
+  }
   return (
     <>
       <Header/> 
       <Switch>
-        <Route path="/yn/" exact={true} component={Main}/>
+        <Route path="/yn" exact={true} component={Main}/>
         <Route path="/yn/about" component={About}/>
         <Route path="/yn/contact" component={Contact}/>
         <Route path="/yn/portfolio" component={Portfolio}/>
+        <Route path="/yn/*"><NoMatch/></Route>
+        {/* <Route component={Main} /> */}
       </Switch>
       <Footer/>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
       <h1 className="logo"><Link to="/yn/"><img src={logo} alt="Yn Logo"/></Link></h1>
       <nav className="gnb">
         <ul>
-          <li><Link to="/yn/">HOME</Link></li>
+          <li><Link to="/yn">HOME</Link></li>
           <li><Link to="/yn/about">ABOUT</Link></li>
           <li><Link to="/yn/portfolio">PORTFOLIO</Link></li>
           <li><Link to="/yn/contact">CONTACT</Link></li>
